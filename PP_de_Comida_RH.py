3c01cbd8239fb0bf7e5c283ae95c9db9e0034dea
RodrigoHTEC10 <A01713854@tec.mx>
2024-09-09 20:15:39 -0600

    Avance 4 - Estructuras de decisión
    Changes and additions:
    - Addition of new recipes.
    - Creation of algorithm to print the shopping list in alphabetical order.
    - Commenting to show the decision structures.

--- PP_de_Comida_RH.py
+++ PP_de_Comida_RH.py
@@ -51,6 +51,14 @@ chicken_raft = {
     "": 0,
 }
 
+eggs_tortilla = {
+    "Eggs": 2,
+    "Tortillas (gr)": 50,
+    "Oil (ml)": 10,
+    "Salt (gr)": 0.2,
+    "":0,
+}
+
 french_toast = {
     
     "Eggs":2,
@@ -62,6 +70,23 @@ french_toast = {
     
 }
 
+breakfast_tacos = {
+    "Eggs": 3,
+    "Milk (ml)": 175,
+    "Hot cake mix": 1,
+    "Butter (gr)": 15,
+    "Maple syrup (ml)": 100,
+    "Ham slides":2,
+    "": 0,
+}
+
+eggs_potatoes = {
+    "Eggs":2,
+    "Potatoes":2,
+    "Oil (ml)": 20,
+    "": 0,
+}
+
 # All recipes are collected on a dictionary, in this particular case Breakfast
 BREAKFAST = {
     "Eggs with ham" : eggs_ham,
@@ -70,6 +95,9 @@ BREAKFAST = {
     "Eggs with bacon": eggs_bacon,
     "Chicken on a raft": chicken_raft,
     "French toast": french_toast,
+    "Eggs with tortilla": eggs_tortilla,
+    "Breakfast tacos":breakfast_tacos,
+    "Eggs with potatoes":eggs_potatoes,
 }
 
 
@@ -85,7 +113,7 @@ hamburger = {
     "Tomato" : 1,
     "American cheese":1,
     "Ketchup (gr)": 100,
-    "Mayonaisse (gr)" : 100,
+    "Mayonaisse (gr)" : 50,
     "Mustard (gr)" : 50,
     "": 0,
     
@@ -97,7 +125,7 @@ steak_veggy = {
     "Carrot": 2,
     "Zuccini": 2,
     "Oil (ml)": 50,
-    "Salt (gr)": 0.2,
+    "Salt (gr)": 5,
     "": 0,
 }
 
@@ -110,9 +138,9 @@ steak_onion = {
 
 papas_chorizo = {
     
-    "Papas": 10,
+    "Potatoes": 10,
     "Chorizo (gr)": 250,
-    "Crema (ml)": 100,
+    "Sour cream (ml)": 100,
     "Cheese (gr)": 150,
     "Bolillo": 1,
     "": 0,
@@ -124,21 +152,56 @@ hotdogs = {
     "Sausage": 2,
     "Bacon strips": 2,
     "Ketchup (gr)": 100,
-    "Mayonaisse (gr)" : 100,
+    "Mayonaisse (gr)" : 50,
     "Mustard (gr)" : 50,
     "": 0,
     
 }
 
+tortitas_tuna = {
+    "Eggs":2,
+    "Can of tuna": 2,
+    "Potatoes":6,
+    "":0,
 
 
+}
+
+enchilada_verde = {
+
+    "Tortillas (gr)":100,
+    "Chicken breast (gr)": 700,
+    "Sour cream (ml)": 150,
+    "Cheese (gr)": 250,
+    "Onion": 0.5,
+    "Tomatillos": 10,
+    "Garlic clove":1,
+    "Green chile":1,
+    "Cilantro (gr)": 50,
+    "Oil (ml)": 50,
+    "Salt (gr)": 50,
+    "Baking soda (gr)": 20, 
+
+}
+
+tuna_vegetables = {
+    "Can of tuna": 2,
+    "Can of vegetables":1,
+    "Saladitas crackets package": 0.5,
+    "Mayonaisse (gr)" : 50,
+    "":0,
+
+}
 
 LUNCH = {
     "Hamburger": hamburger,
     "Steak with vegetables": steak_veggy,
     "Steak with onion": steak_onion,
-    "Papas con Chorizo": papas_chorizo,
+    "Potatoes with chorizo": papas_chorizo,
     "Hotdogs" : hotdogs,
+    "Little tuna tortas": tortitas_tuna,
+    "Green enchiladas":enchilada_verde,
+    "Tuna with vegetables":tuna_vegetables,
 }
 
 
@@ -188,8 +251,8 @@ DINNER = {
     "Molletes": molletes,
     "Quesadillas": quesadillas,
     "Cereal": cereal,
-    "Frijoles con chorizo": frijoles_chorizo,
-    "Nopales con sombrero": nopales_sombrero,
+    "Beans with chorizo": frijoles_chorizo,
+    "Nopales with hat": nopales_sombrero,
 }
 
 
@@ -205,13 +268,22 @@ calories = {
     "Steak with onion": 198,
     "Molletes": 510,
     "Quesadillas": 693,
-    "Papas con Chorizo": 302,
+    "Potatoes with chorizo": 302,
     "Cereal": 420,
-    "Frijoles con chorizo":334,
-    "Nopales con sombrero": 378,
+    "Beans with chorizo":334,
+    "Nopales with hat": 378,
     "Chicken on a raft": 210,
     "Hotdogs" : 468,
-    "French toast": 356
+    "French toast": 356,
+    "Little tuna tortas":592,
+    "Eggs with tortilla":395,
+    "Breakfast tacos": 930,
+    "Green enchiladas": 1018,
+    "Eggs with potatoes":378,
+    "Tuna with vegetables":115,
+
+
+
 }
 
 
@@ -235,16 +307,16 @@ def get_ingredients(meal_type1, meal_list1, meal_type2, meal_list2, meal_type3, 
         temporal = meal_type1.get(x)
         is_new = False
         for y in temporal.keys():
-            if y in shop_list and is_new == True:
+            if y in shop_list and is_new == True:                       #if
                 is_new = False
 
-            elif y in shop_list and is_new == False:
+            elif y in shop_list and is_new == False:                    #elif
                 quantity = shop_list.get(y)
                 new_quantity = temporal.get(y)
                 shop_list.update({y: quantity+new_quantity})            #Math Operation (Addition)
 
-            else:
-                new_quantity = temporal.get(y)
+            else:                                                       #else
+                new_quantity = temporal.get(y)                          
                 shop_list.update({y:new_quantity})
                 is_new = True
 
@@ -253,15 +325,15 @@ def get_ingredients(meal_type1, meal_list1, meal_type2, meal_list2, meal_type3, 
         temporal = meal_type2.get(x)
         is_new = False  
         for y in temporal.keys():
-            if y in shop_list and is_new == True:
+            if y in shop_list and is_new == True:                       #if
                 is_new = False
 
-            elif y in shop_list and is_new == False:
+            elif y in shop_list and is_new == False:                    #elif
                 quantity = shop_list.get(y)
                 new_quantity = temporal.get(y)
                 shop_list.update({y: quantity+new_quantity})            #Math Operation (Addition)
 
-            else:
+            else:                                                       #else
                 new_quantity = temporal.get(y)
                 shop_list.update({y:new_quantity})
                 is_new = True
@@ -271,16 +343,16 @@ def get_ingredients(meal_type1, meal_list1, meal_type2, meal_list2, meal_type3, 
         temporal = meal_type3.get(x)
         is_new = False
         for y in temporal.keys():
-            if y in shop_list and is_new == True:
+            if y in shop_list and is_new == True:                       #if
                 is_new = False
 
-            elif y in shop_list and is_new == False:
+            elif y in shop_list and is_new == False:                    #elif
                 quantity = shop_list.get(y)
                 new_quantity = temporal.get(y)
                 shop_list.update({y: quantity+new_quantity})            #Math Operation (Addition)
                 is_new = False
 
-            else:
+            else:                                                       #else
                 new_quantity = temporal.get(y)
                 shop_list.update({y:new_quantity})
                 is_new = True
@@ -294,15 +366,15 @@ def get_ingredients(meal_type1, meal_list1, meal_type2, meal_list2, meal_type3, 
 def calories_count(meal_list1, meal_list2, meal_list3):
     calories_total = 0.0
     for x in meal_list1:
-        if x in calories.keys():
+        if x in calories.keys():                                        #if
             calories_total += calories.get(x)                           #Math Operation (Addition)
     
     for x in meal_list2:
-        if x in calories.keys():
+        if x in calories.keys():                                        #if
             calories_total += calories.get(x)                           #Math Operation (Addition)
 
     for x in meal_list3:
-        if x in calories.keys():
+        if x in calories.keys():                                        #if
             calories_total += calories.get(x)                           #Math Operation (Addition)
 
     return calories_total
@@ -320,13 +392,13 @@ def print_menu(meal_list1, meal_list2, meal_list3,num_days):
         print(f"    Lunch:      {meal_list2[x-1]}")
         print(f"    Dinner:     {meal_list3[x-1]}")
 
-        if meal_list1[x-1] in calories.keys():
+        if meal_list1[x-1] in calories.keys():                          #if
             calories_day += calories.get(meal_list1[x-1])               #Math Operation (Addition)
 
-        if meal_list2[x-1] in calories.keys():
+        if meal_list2[x-1] in calories.keys():                          #if
             calories_day += calories.get(meal_list2[x-1])               #Math Operation (Addition)
 
-        if meal_list3[x-1] in calories.keys():
+        if meal_list3[x-1] in calories.keys():                          #if
             calories_day += calories.get(meal_list3[x-1])               #Math Operation (Addition)
 
         print(f"    Calories of the day: {calories_day}")
@@ -337,9 +409,13 @@ def print_menu(meal_list1, meal_list2, meal_list3,num_days):
 
 #Function print_list (shop_list) Prints the shopping list in a stylish format.
 def print_list(list):
+    new_list = []
     for k,v in list.items():
-        if k!=""and v!=0:
-            print(f"[ ] {k}: {v}")
+        if k!=""and v!=0:                                               #if
+            new_list.append(f"[ ] {k}: {v}")
+    sorted_list = sorted(new_list)
+    for x in sorted_list:
+        print(x)
 
 
 
@@ -349,7 +425,7 @@ def print_list(list):
 #Introduccion and input from the user to start the program.
 print("Welcome to the Meal Planner and Shopping List Generator.\nWould you like to get the meals of this week?")
 order = input("Please answer \"Yes\" or \"No\": ")
-if order == "Yes" or "yes":
+if order == "Yes" or "yes":                                             #if
     #Input of number of days to call the functions get meal from each category.
     num_days = int(input("Please enter the number of days you would like to receive a Meal Plan: "))
     meals_b =get_meal(BREAKFAST,num_days)
@@ -363,7 +439,7 @@ if order == "Yes" or "yes":
     print_list(shoppy)
     print(f"The total calories of this week are: {menu_total_cal}")
     
-else:
+else:                                                                   #else
     print("Thank you for using the program. Have a good day.")
 
 
